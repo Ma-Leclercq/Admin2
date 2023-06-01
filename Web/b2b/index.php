feacedf98f70967aad5a2e4854245681a2fe4b18
Louis Guiot <75644069+burgersnake11@users.noreply.github.com>
2023-06-01 12:29:32 +0200

    Add web server files

---
+++ Web/b2b/index.php
@@ -0,0 +1,35 @@
+<?php
+$mysqli = new mysqli("172.17.0.4:3306",'root','Frimouche','woodytoys_db');
+
+// Check connection
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+
+?>
+
+<html>
+
+<head>
+<title>WoodyToys - B2B</title>
+</head>
+
+<body>
+    <h1>Bienvenue sur le site B2B de WoodyToys!</h1>
+    <h2>Veuillez trouver ci-dessous la liste de nos produits :</h2>
+    <?php
+        $query = "SELECT * FROM toys;";
+        mysqli_query($mysqli, $query) or die('Error querying database.');
+        $result = mysqli_query($mysqli, $query);
+
+        while ($row = mysqli_fetch_array($result)) {
+            echo '<li>' . $row['toyName'] . ' - ' . $row['toyPrice'] . ' € </li>';
+        }
+    ?>
+    <!--echo $row['id_toys'] . ': ' . $row['toyName'] . ' - ' . $row['toyPrice'] . ' € <br />' -->
+    <p>N'hésitez-pas à nous contacter par mail pour plus d'informations - he201974@students.ephec.be</p>
+    <p>© WoodyToys 2023 - Tous droits réservés.</p>
+</body>
+
+</html>
